9d45a6c0ba2e0484dba3a26c1c7507a12beec5b4
Sasha Bayan <sasha_bayan@apple.com>
2016-06-07 21:21:22 -0700

    add style changes

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,8 @@ import CriteriaButtons from './CriteriaButtons/CriteriaButtons';
 import SubmitButtonContainer from './SubmitButtonContainer/SubmitButtonContainer';
 import CentersContainer from './CentersContainer/CentersContainer';
 import { Col, Row } from 'react-bootstrap';
+import styles from './Main.scss';
+import CSSModules from 'react-css-modules'
 
 import needs from '../data/needs.json';
 import languages from '../data/languages.json';
@@ -11,17 +13,24 @@ import insurances from '../data/insurances.json';
 // TODO: wrap App in routes when routing is implemented
 // import { Link, RouteHandler } from 'react-router';
 
-@connect(mapStateToProps)
 class App extends React.Component {
   render() {
     return (
       <div>
         <Row>
           <Col xs={3}>
-            <CriteriaButtons criteria={needs} type="need" />
-            <CriteriaButtons criteria={languages} type="language" />
-            <CriteriaButtons criteria={insurances} type="insurance" />
-            <SubmitButtonContainer> Submit </SubmitButtonContainer>
+            <div styleName="need">
+              <CriteriaButtons criteria={needs} type="need" />
+            </div>
+            <div styleName="language">
+              <CriteriaButtons criteria={languages} type="language" />
+            </div>
+            <div styleName="insurance">
+              <CriteriaButtons criteria={insurances} type="insurance" />
+            </div>
+            <div styleName="submit">
+              <SubmitButtonContainer> Submit </SubmitButtonContainer>
+            </div>
           </Col>
           <Col xs={9}>
             <CentersContainer />
@@ -38,4 +47,5 @@ function mapStateToProps(state) {
   };
 }
 
-export default App;
+App = CSSModules(App, styles, { errorWhenNotFound: false });
+export default connect(mapStateToProps)(App);
